e8cc2fca73a350b7e113bf06c730672b2a3eaaae
Marc Monserrat Núñez <mmonserrat90@gmail.com>
2020-06-08 12:33:56 +0700

    link to spotify

--- src/components/song/SongTable.tsx
+++ src/components/song/SongTable.tsx
@@ -2,6 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const SongTable = ({songs}) => {
+
+  function openSpotifyTrackLink(spotifyTrackId) {
+    let win = window.open('https://open.spotify.com/track/' + spotifyTrackId, '_blank');
+    win.focus();
+  }
+
   return (
     <div>
       <table className="table table-dark">
@@ -11,15 +17,23 @@ const SongTable = ({songs}) => {
           <th scope="col">Artist</th>
           <th scope="col">Track</th>
           <th scope="col">Date</th>
+          <th scope="col"/>
         </tr>
         </thead>
         <tbody>
         {songs.map((s, i) => (
           <tr key={i}>
-            <th scope="row">{i+1}</th>
+            <th scope="row">{i + 1}</th>
             <td>{s.artists}</td>
             <td>{s.track}</td>
             <td>{(new Date(s.date)).toLocaleString()}</td>
+            <td>
+              {s.spotifyTrackId ?
+                <a href='#' onClick={() => openSpotifyTrackLink(s.spotifyTrackId)} className='btn-link'>
+                  <i className='fa fa-spotify' title='Spotify'/>
+                </a>
+                : ''}
+            </td>
           </tr>
         ))}
         </tbody>
