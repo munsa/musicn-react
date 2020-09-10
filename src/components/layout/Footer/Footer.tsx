1844ea7ae66ac958da51debc6d6bd980870862e3
Marc Monserrat Núñez <mmonserrat90@gmail.com>
2020-09-10 22:37:12 +0200

    footer logo

--- src/components/layout/Footer/Footer.tsx
+++ src/components/layout/Footer/Footer.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import './Footer.css';
-import {Col, NavDropdown, Row} from 'react-bootstrap';
+import {Col, Row} from 'react-bootstrap';
+import GeoTunes from '../../../shared/assets/image/logo-gt.png';
 
 const Footer = () => {
   return (
     <div className='footer mt-5'>
       <Row className='footer-content'>
         <Col xs={6} className='footer-brand'>
-          GeoTunes
+          <img src={GeoTunes} className='footer-brand-logo'/>
         </Col>
         <Col xs={6} className='footer-github'>
           <a href='https://github.com/munsa' target="_blank" rel="noopener noreferrer">
