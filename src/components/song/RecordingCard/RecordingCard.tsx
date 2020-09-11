803a0e4e218ccf4b663652c593b56cbf2b54b517
Marc Monserrat Núñez <mmonserrat90@gmail.com>
2020-09-11 15:07:44 +0200

    responsive buttons in card

--- src/components/song/RecordingCard/RecordingCard.tsx
+++ src/components/song/RecordingCard/RecordingCard.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {Button, Card} from 'react-bootstrap';
+import {Card} from 'react-bootstrap';
 import getArtistsString from '../../../shared/utils/StringUtils';
 import './RecordingCard.css'
 import RecordImage from '../../../shared/assets/image/record_400.png'
@@ -10,20 +10,27 @@ import DeezerButton from '../../../shared/lib/buttons/DeezerButton/DeezerButton'
 const RecordingCard = ({recording}) => {
   return (
     <Card className='recording-card'>
-      <div>
+      <div className='recording-card-container'>
         <img className='recording-card-image' src={RecordImage}/>
-      </div>
-      <div className='recording-card-content'>
-        <div className='recording-card-track text-truncate'>
-          {recording.acrCloud?.track?.name}
+
+        <div className='recording-card-content'>
+          <div className='recording-card-track text-truncate'>
+            {recording.acrCloud?.track?.name}
+          </div>
+          <div className='recording-card-artists text-truncate'>
+            {getArtistsString(recording.acrCloud?.artists)}
+          </div>
         </div>
-        <div className='recording-card-artists text-truncate'>
-          {getArtistsString(recording.acrCloud?.artists)}
-        </div>
       </div>
-      <div className='recording-card-buttons'>
-        <SpotifyButton/>
-        <DeezerButton/>
+      <div className='recording-card-buttons mx-auto mx-2'>
+        <div className='d-flex flex-wrap justify-content-center'>
+          <div className='card-button-container p-2'>
+            <SpotifyButton/>
+          </div>
+          <div className='card-button-container p-2'>
+            <DeezerButton/>
+          </div>
+        </div>
       </div>
     </Card>
   )
