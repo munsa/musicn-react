31b235743cd1e1592157ec00c5bf4b5b04524a4f
Marc Monserrat Núñez <mmonserrat90@gmail.com>
2020-09-11 17:22:09 +0200

    source common button

--- src/components/song/RecordingCard/RecordingCard.tsx
+++ src/components/song/RecordingCard/RecordingCard.tsx
@@ -4,16 +4,17 @@ import {Card} from 'react-bootstrap';
 import getArtistsString from '../../../shared/utils/StringUtils';
 import './RecordingCard.css'
 import RecordImage from '../../../shared/assets/image/record_400.png'
-import SpotifyButton from '../../../shared/lib/buttons/SpotifyButton/SpotifyButton';
-import DeezerButton from '../../../shared/lib/buttons/DeezerButton/DeezerButton';
+import SourceButton from '../../../shared/lib/buttons/SourceButton/SourceButton';
+import {Source} from '../../../shared/constants/constants';
 
-const openSpotifyTrackLink = spotifyTrackId => {
-  let win = window.open('https://open.spotify.com/track/' + spotifyTrackId, '_blank');
-  win.focus();
-}
-
-const openDeezerTrackLink = deezerTrackId => {
-  let win = window.open('https://www.deezer.com/track/' + deezerTrackId, '_blank');
+const openSourceTrackURL = (source: string, trackId:string) => {
+  let url: string;
+  if(source === Source.SPOTIFY) {
+    url = 'https://open.spotify.com/track/' + trackId;
+  } else if (source === Source.DEEZER) {
+    url = 'https://www.deezer.com/track/' + trackId;
+  }
+  let win = window.open(url, '_blank');
   win.focus();
 }
 
@@ -36,12 +37,12 @@ const RecordingCard = ({recording}) => {
         <div className='d-flex flex-wrap justify-content-center'>
           {recording.spotify?.track?.id != null &&
           <div className='p-2'>
-            <SpotifyButton onClickCallback={() => openSpotifyTrackLink(recording.spotify.track.id)}/>
+            <SourceButton source={Source.SPOTIFY} onClickCallback={() => openSourceTrackURL(Source.SPOTIFY, recording.spotify.track.id)}/>
           </div>
           }
           {recording.deezer?.track?.id != null &&
           <div className='p-2'>
-            <DeezerButton onClickCallback={() => openDeezerTrackLink(recording.deezer.track.id)}/>
+            <SourceButton source={Source.DEEZER} onClickCallback={() => openSourceTrackURL(Source.DEEZER, recording.deezer.track.id)}/>
           </div>
           }
         </div>
