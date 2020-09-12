26c9fbbc1fc583fc488ef44c2b872ccd47850655
Marc Monserrat Núñez <mmonserrat90@gmail.com>
2020-09-12 14:13:52 +0200

    font awesome

--- src/components/layout/Footer/Footer.tsx
+++ src/components/layout/Footer/Footer.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import './Footer.css';
 import {Col, Row} from 'react-bootstrap';
 import WildTunes from '../../../shared/assets/image/wildtunes/logo/wildtunes_logo_small_white.svg';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faWrench} from '@fortawesome/free-solid-svg-icons';
+import {faGithub, faNodeJs, faReact, faBootstrap, faAws} from '@fortawesome/free-brands-svg-icons';
 
 const Footer = () => {
   return (
@@ -11,11 +14,11 @@ const Footer = () => {
           <img src={WildTunes} className='footer-brand-logo'/>
         </Col>
         <Col xs={8} className='footer-message'>
-            <i className='fa fa-wrench' title='Construction'/> WildTunes is in a development stage. This is a demo.
+          <FontAwesomeIcon icon={faWrench}/> WildTunes is in a development stage. This is just a demo :)
         </Col>
         <Col xs={2} className='footer-github'>
           <a href='https://github.com/munsa' target="_blank" rel="noopener noreferrer">
-            <i className='fa fa-github' title='Github'/>
+            <FontAwesomeIcon icon={faNodeJs}/> <FontAwesomeIcon icon={faReact}/> <FontAwesomeIcon icon={faBootstrap}/> <FontAwesomeIcon icon={faAws}/> <FontAwesomeIcon icon={faGithub}/>
           </a>
         </Col>
       </Row>
