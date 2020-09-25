2a043540df23120ba7ca05003a0bc1b26e96b21f
Marc Monserrat Núñez <mmonserrat90@gmail.com>
2020-09-25 12:07:07 +0200

    landing blank component + routing

---
+++ src/components/layout/Landing/Landing.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Landing.css';
+
+const Landing = () => {
+  return (
+    <div>
+      Hello World! You are not logged in
+    </div>
+  )
+}
+
+export default Landing;
